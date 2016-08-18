5ca930c8b638947531568eff3d7f0876ac3bf39d
Sarthak Khillon <sarthakkhillon24@gmail.com>
2016-08-18 01:14:44 -0700

    Installed pod 'OpenCV'
    xcworkspace

---
+++ Pods/Target Support Files/Pods-Photo2Contacts/Pods-Photo2Contacts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Photo2Contacts : NSObject
+@end
+@implementation PodsDummy_Pods_Photo2Contacts
+@end
---
+++ Pods/Target Support Files/Pods-Photo2Contacts/Pods-Photo2Contacts-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Photo2ContactsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Photo2ContactsVersionString[];
+
